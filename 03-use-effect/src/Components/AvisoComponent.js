2b85e75196b795ad1f140ecfafe8b030e8d5bf42
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-08-10 22:57:03 -0400

    se añaden cambios en los ejercicio de useEffect

---
+++ 03-use-effect/src/Components/AvisoComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const AvisoComponent = () => {
+  return (
+    <div>
+        <h1>Hemos superado los 15 cambios</h1>
+        <button>Mostrar alerta</button>
+    </div>
+  )
+}
--- 03-use-effect/src/Components/PruebasComponent.js
+++ 03-use-effect/src/Components/PruebasComponent.js
@@ -1,24 +1,49 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import { AvisoComponent } from './AvisoComponent';
 
 export const PruebasComponent = () => {
 
     const [usuario, setUsuario]= useState("Alberto Aguilera")
+    const [fecha, setFecha]= useState("31-01-1987")
+    const [contador, setContador]= useState(0);
 
     const modUsuario = e =>{
         setUsuario(e.target.value)
     }
 
+    const cambiarFecha= e =>{
+      setFecha(new Date().toLocaleDateString());
+    }
+
+    //los corches hacen que el useEffect solo se ejecute 1 vez
+    useEffect(()=>{
+      console.log("has cargado el componente PruebasComponent");
+    }, []);
+
+    useEffect(()=>{
+      setContador(contador+1);
+      console.log("has modificado el usuario: "+contador);
+
+    }, [fecha, usuario]);
+
   return (
     <div>
         <h1>El efecto - Hook useEffect</h1>
-        <strong className='label'>{usuario}</strong>
-        <form>
+        <strong className={contador >= 10 ? 'label label-green' : 'label'}>{usuario}</strong>
+        <strong>{fecha}</strong>
+
+        <p>
         <input 
         type='text' 
         onChange={modUsuario} 
         placeholder='Cambia el nombre'
         />
-        </form>
+
+        <button onClick={cambiarFecha}>Cambiar Fecha</button>
+        </p>
+
+        {contador >= 15 && <AvisoComponent/>}
+
     </div>
   )
 }
