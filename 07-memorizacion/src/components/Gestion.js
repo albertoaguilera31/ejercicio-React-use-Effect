fbb2c82b56d94c1aaa145e5dc77b05f055648f3e
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-09-01 23:07:03 -0400

    se crea la carpeta para el hook useMemo y su practica

---
+++ 07-memorizacion/src/components/Gestion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Gestion = () => {
+  return (
+    <div>
+        <h1>Nombre del gestor: Alberto</h1>
+
+        <h2>Listado de empleados</h2>
+    </div>
+  )
+}
