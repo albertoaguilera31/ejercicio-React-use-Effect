8848f8cb076866ab4b41b3870246bb3bacade5af
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-09-03 23:16:16 -0300

    se avanza en el estudio de los hook con el hookmemo

--- 07-memorizacion/src/App.js
+++ 07-memorizacion/src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 import { Gestion } from './components/Gestion';
+import { Tareas } from './components/Tareas';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
+      {/*Ejercicio con hook useMemo*/}
+      <Tareas/>
+
+        {/*ejercicio con metodo memo para componentes*/}
         {/*<Gestion/>*/}
       </header>
     </div>
--- 07-memorizacion/src/components/Tareas.js
+++ 07-memorizacion/src/components/Tareas.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 export const Tareas = () => {
   return (
-    <div>
+    <div className='tareas-container'>
         <h1>Mis tareas</h1>
         <form>
             <input type='text' name='descripcion' placeholder='Describe la tarea'/>
             <input type='submit' value='guardar'/>
         </form>
+
+        <h3>Lista de Tareas:</h3>
     </div>
   )
 }
